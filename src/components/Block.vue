4c83c7b9a3143592344ffcfbc273a7773f372cd2
andilekwazitp@gmail.com <andile.msomi@mondiadigital.com>
2024-10-10 12:36:10 +0200

    second  commit

---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="block"> Click </div>
+</template>
+
+<script>
+export default {
+props:['delay']
+}
+</script>
+
+<style>
+.block{
+    width:400px;
+    border-radius: 35px;
+    background:grey;
+    color:white;
+    text-align: center;
+    margin:40px auto;
+    padding: 100px 0;
+}
+</style>
