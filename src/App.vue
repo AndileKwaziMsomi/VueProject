d8eee9c97477128472f4439d0ccd6aef3e6546d1
andilekwazitp@gmail.com <andile.msomi@mondiadigital.com>
2024-10-14 08:59:02 +0200

    Updating my reaction time code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <h1> Hey Lets play a game</h1>
- <button @click="start" :disabled="isPlaying"> Press Play</button>
- <block v-if="isPlaying" :delay="delay"/>
+  <div>
+    <h1>Hey, Let's play a game</h1>
+    <button @click="start" :disabled="isPlaying">Press Play</button>
+    <div>
+      <button @click="resetGame" :disabled="!isPlaying">Reset</button>
+    </div>
+    <!-- Block only appears if the game is playing -->
+    <block v-if="isPlaying" :delay="delay" @blockHidden="endGame" />
+  </div>
 </template>
 
 <script>
@@ -9,23 +15,27 @@ import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: { Block},
-    data(){
-      return{
-        isPlaying: false,
-        delay: null
-      }
-    },
-    methods:{
-    start(){
-      this.delay = 2000 + Math.random() * 5000  
-      this.isPlaying = true
-      
-    }
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    };
   },
-  
-  
-}
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+    resetGame() {
+      this.isPlaying = false;  // Stop the game
+      this.delay = null;       // Reset the delay
+    },
+    endGame() {
+      this.isPlaying = false;  // End the game when block is hidden
+    }
+  }
+};
 </script>
 
 <style>
@@ -36,5 +46,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  border: none;
+  background-color: #58626d;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:disabled {
+  background-color: #b0b0b0;
 }
 </style>
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,21 +1,46 @@
 <template>
-  <div class="block"> Click </div>
+  <div class="block" v-if="showBlock" @click="stopTimer"> Click
+  </div>
 </template>
 
 <script>
 export default {
-props:['delay']
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false, // Initially false so the block doesn't appear twice
+      startTime: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true; // Show the block after the delay
+      this.startTimer();  
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.startTime = Date.now();
+    },
+    stopTimer() {
+      const endTime = Date.now();
+      this.reactionTime = endTime - this.startTime;
+      this.showBlock = false; // Hide the block after the click
+    }
+  }
 }
 </script>
 
 <style>
-.block{
-    width:400px;
-    border-radius: 35px;
-    background:grey;
-    color:white;
-    text-align: center;
-    margin:40px auto;
-    padding: 100px 0;
+.block {
+  width: 400px;
+  border-radius: 35px;
+  background: grey;
+  color: white;
+  text-align: center;
+  margin: 40px auto;
+  padding: 100px 0;
+  cursor: pointer;
 }
 </style>
